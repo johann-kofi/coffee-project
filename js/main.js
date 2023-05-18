74ccdfedcf132db887493b010da6eeed48597956
Kof'i Knowles <JemalKnowles1@gmail.com>
2023-05-18 12:02:28 -0500

    Updated Proj

---
+++ js/main.js
@@ -0,0 +1,93 @@
+"use strict";
+
+(function () {
+
+    function renderCoffee(coffee) {
+        let html = "";
+        let fontRoastClass = `font-color-${coffee.roast}`;
+        html = '<div class="coffee col-6">';
+        html += `<div><span class="fs-3 ${fontRoastClass}">${coffee.name}</span>`;
+        html += ' <span class="fs-5">' + coffee.roast + '</span></div>';
+        html += '</div>';
+
+        return html;
+    }
+
+    function renderCoffees(coffees) {
+        let html = '';
+        for (let i = coffees.length - 1; i >= 0; i--) {
+            html += renderCoffee(coffees[i]);
+        }
+        return html;
+    }
+
+    function updateCoffees(e) {
+        e.preventDefault();// don't submit the form, we just want to update the data
+        console.log(e.type);
+        let selectedRoast = roastSelection.value;
+        let filteredCoffees = [];
+        if (selectedRoast === "All") {
+            tbody.innerHTML = renderCoffees(coffees);
+        } else {
+            coffees.forEach(function (coffee) {
+                if (coffee.roast === selectedRoast) {
+                    filteredCoffees.push(coffee);
+                }
+            });
+            tbody.innerHTML = renderCoffees(filteredCoffees);
+        }
+    }
+    function getCoffeesByRoast(arrCoffees, roastType) {
+        let newCoffees = []
+        if (roastType === "All") {
+            tbody.innerHTML = renderCoffees(coffees);
+        } else {
+            coffees.forEach(function (coffee) {
+                if (coffee.roast === roastType) {
+                    newCoffees.push(coffee);
+                }
+            });
+            tbody.innerHTML = renderCoffees(filteredCoffees);
+        }
+
+    }
+
+    function coffeeNames(filteredCoffees) {
+        let nameInput = nameSearch.value;
+        let filteredCoffeesByName = [];
+        filteredCoffees.forEach(function (coffee) {
+            if (coffee.name.toLowerCase().includes(nameInput.toLowerCase())) {
+                filteredCoffeesByName.push(coffee)
+            }
+        });
+        return filteredCoffeesByName
+    }
+
+// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+    let coffees = [
+        {id: 1, name: 'Light City', roast: 'Light'},
+        {id: 2, name: 'Half City', roast: 'Light'},
+        {id: 3, name: 'Cinnamon', roast: 'Light'},
+        {id: 4, name: 'City', roast: 'Medium'},
+        {id: 5, name: 'American', roast: 'Medium'},
+        {id: 6, name: 'Breakfast', roast: 'Medium'},
+        {id: 7, name: 'High', roast: 'Dark'},
+        {id: 8, name: 'Continental', roast: 'Dark'},
+        {id: 9, name: 'New Orleans', roast: 'Dark'},
+        {id: 10, name: 'European', roast: 'Dark'},
+        {id: 11, name: 'Espresso', roast: 'Dark'},
+        {id: 12, name: 'Viennese', roast: 'Dark'},
+        {id: 13, name: 'Italian', roast: 'Dark'},
+        {id: 14, name: 'French', roast: 'Dark'},
+    ];
+
+    let tbody = document.querySelector('#coffees');
+    let submitButton = document.querySelector('#submit');
+    let roastSelection = document.querySelector('#roast-selection');
+    let nameSearch = document.querySelector("#name-search")
+    tbody.innerHTML = renderCoffees(coffees);
+
+    submitButton.addEventListener('click', updateCoffees);
+    roastSelection.addEventListener("change", updateCoffees);
+    nameSearch.addEventListener('input', updateCoffees);
+})();
